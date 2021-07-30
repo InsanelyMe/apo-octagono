0a1a72fa69348bf20dedde44b75c5859ecbd984d
Angel <5618619+InsanelyMe@users.noreply.github.com>
2021-07-29 21:38:04 -0500

    0.75.7
    Forgot these two...
    - Fixed a parameter in UI so that clicking on the navbar will correctly scroll each section into view
    - Changed a few properties for a more readable style

--- ui.js
+++ ui.js
@@ -4,35 +4,31 @@ let navButts = document.getElementsByClassName("navBarButton");
 for (let butt of navButts) {
   butt.onclick = () => {
     var target;
-    console.log(butt.id);
+    // console.log(butt.id);
     switch (butt.id) {
       case "sectionOneButton":
         target = document.getElementById("sectionOne");
-        target.scrollIntoView();
         break;
       case "sectionTwoButton":
         target = document.getElementById("sectionTwo");
-        target.scrollIntoView(false);
         break;
       case "sectionThreeButton":
         target = document.getElementById("sectionThree");
-        target.scrollIntoView(false);
         break;
       case "sectionFourButton":
         target = document.getElementById("sectionFour");
-        target.scrollIntoView();
         break;
       default:
         console.log("mananged to click a button that doesn't exist...");
     }
-    // target.scrollIntoView(false);
+    target.scrollIntoView();
   }
 }
 
 let logoButt = document.getElementById("navBarLogo");
 
 logoButt.onclick = () => {
-  console.log(logoButt.id);
+  // console.log(logoButt.id);
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
